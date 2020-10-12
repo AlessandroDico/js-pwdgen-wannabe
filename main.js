54865e8c456158faab2fafa3709d77d410f28068
Ale <alessandrochb@gmail.com>
2020-10-12 14:51:43 +0200

    aggiunte variabili funzionanti nel main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-document.writeln('hello world');
+// document.writeln('hello world');
+
+var nome_utente = prompt("qual'è il tuo nome?");
+
+console.log(nome_utente);
+
+var cognome_utente = prompt("qual'è il tuo cognome?");
+
+console.log(cognome_utente);
+
+var colore_preferito = prompt("qual'è il tuo colore preferito?");
+
+console.log(colore_preferito);
+
+var password_perfetta = nome_utente + cognome_utente + colore_preferito + "20";
+
+console.log(password_perfetta);
+
+document.getElementById('password_sicurissima').innerHTML = password_perfetta;
